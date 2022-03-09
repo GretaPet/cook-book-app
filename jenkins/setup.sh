d0d9c029e69585dceecfb6fe636a2963eec10023
Ubuntu <greta@VM01.gcxglha5sm5evnllvlsq3ynp0g.zx.internal.cloudapp.net>
2022-03-09 20:43:34 +0000

    Setup stage for jenkinsfile 01

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+echo "Setup stage"
 
 sudo apt-get update
-sudo apt-get install python3 python3-venv python3-pip
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
+sudo apt-get install -y curl jq
 
-python3 create.py
-python3 app.py
+curl https://get.docker.com | sudo bash
+
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
+
