dc8bd7fb10c879e6875a4989bd92ff24cf91e51d
Greta <97620006+GretaPet@users.noreply.github.com>
2022-03-09 14:28:21 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install python python-venv python-pip
-python -m venv venv
-source venv/Scripts/activate
-pip install -r requirements.txt
+sudo apt install python3 python3-venv python3-pip
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 
 python3 create.py
 python3 app.py
