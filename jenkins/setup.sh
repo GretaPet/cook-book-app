2d39670e7c5db66e89ac72f4445623d04db892ca
Greta <97620006+GretaPet@users.noreply.github.com>
2022-03-10 18:37:56 +0000

    Update setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -4,7 +4,10 @@ echo "Setup stage"
 sudo apt-get update
 sudo apt-get install -y curl jq
 
-curl https://get.docker.com | sudo bash
+if [ ! -f "/usr/bin/docker" ]; then
+    curl https://get.docker.com | sudo bash
+    sudo usermod -aG docker jenkins
+fi
 
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
