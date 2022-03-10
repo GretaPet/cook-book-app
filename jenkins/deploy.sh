e07ea94594284c0db4afe73d9d8574f11d3f11a6
Greta <97620006+GretaPet@users.noreply.github.com>
2022-03-10 15:40:08 +0000

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Deployment stage"
 
-scp docker-compose.yaml jenkins@swarm-manager/home/jenkins/docker-compose.yaml
+scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh jenkins@swarm-manager \
   DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
   docker stack deploy --compose-file docker-compose.yaml cook-book-app
