d450a3f027c8b76492bf15ae07d36092954f1308
Greta <97620006+GretaPet@users.noreply.github.com>
2022-03-10 19:05:22 +0000

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -9,7 +9,7 @@ pip3 install pytest flask_testing
 pip3 install -r requirements.txt
 
 
-python3 -m pytest cook-book-app/application \
+python3 -m pytest application \
   --cov=application \
   --cov-report term-missing \
   --cov-report xml:coverage.xml \
