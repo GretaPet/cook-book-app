5af661a9a829fd28e89a0750b95598294d6c2eea
Greta <97620006+GretaPet@users.noreply.github.com>
2022-03-10 16:48:37 +0000

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -6,8 +6,7 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip3 install pytest pytest-cov flask_testing requests_mock
-pip3 install -r frontend/requirements.txt
-pip3 install -r backend/requirements.txt
+pip3 install -r requirements.txt
 
 mkdir test_reports
 
