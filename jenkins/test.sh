81b47bb2c65a29093dc0e2353a7ceb4a6165b7a3
Greta <97620006+GretaPet@users.noreply.github.com>
2022-03-10 16:45:52 +0000

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 echo "This is test stage"
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install pytest pytest-cov flask_testing requests_mock
+pip3 install -r frontend/requirements.txt
+pip3 install -r backend/requirements.txt
+
+mkdir test_reports
+
+python3 -m pytest \
+  --cov \
+  --cov=application \
+  --cov-report term-missing \
+  --cov-report xml:coverage.xml \
+  --junitxml=junit_report.xml
+
+deactivate
+rm -rf venv
