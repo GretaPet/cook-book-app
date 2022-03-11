2df93e11656a8e7afb471c5dafbb7b37b8ef18eb
Greta <97620006+GretaPet@users.noreply.github.com>
2022-03-11 09:27:36 +0000

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 from os import name
 from flask import url_for
 from flask_testing import TestCase
-from application import app
+from application import app, db
 from application.models import Dishes
 
 class TestBase(TestCase):
@@ -12,6 +12,24 @@ class TestBase(TestCase):
             DEBUG=True,
             WTF_CSRF_ENABLED=False)
         return app
+
+    def setUp(self):
+
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+        dish1=Dishes(name='pizza')
+        dish2=Dishes(name='pasta')
+
+        db.session.add(dish1)
+        db.session.add(dish2)
+        db.session.commit()
+
+    def tearDown(self):
+        
+        db.session.remove()
+        db.drop_all()
 
 class TestDishes(TestBase):
 
@@ -24,3 +42,6 @@ class TestDishes(TestBase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'pizza', response.data)
 
+    def test_home(self):
+        response = self.client.get(url_for('home'))
+        self.assertEqual(response.status_code, 200)
