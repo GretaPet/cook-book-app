91d8641ed1e34e3092780bd580c4af6016e6af70
Greta <97620006+GretaPet@users.noreply.github.com>
2022-03-11 09:16:15 +0000

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -5,7 +5,7 @@ echo "This is test stage"
 python3 -m venv venv
 source venv/bin/activate
 
-pip3 install pytest pytest-cov flask_testing
+pip3 install pytest pytest-cov flask_testing wheel
 pip3 install -r requirements.txt
 
 python3 -m pytest application \
