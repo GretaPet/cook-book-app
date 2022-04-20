e35f3f813b49c9466547b95f5f2c476c6c9051bd
GretaPet <greta.petreviciute@gmail.com>
2022-04-20 09:10:07 +0100

    Relationship between dishes and ingreedients created

--- application/models.py
+++ application/models.py
@@ -1,6 +1,15 @@
 from application import db
 
+dish_ingr= db.Table('dish_ingr',
+    db.Column('dishes_id', db.Integer, db.ForeignKey('dishes.id')),
+    db.Column('ingreedient_id', db.Integer, db.ForeignKey('ingreedient.id'))
+)
+
 class Dishes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    
+
+class Ingreedient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    recipe = db.relationship('Dishes', secondary=dish_ingr, backref='recipes')
